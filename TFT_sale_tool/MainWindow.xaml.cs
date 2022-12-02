376dbd03790f802022b356feedbb40b400e7c807
Skuamato <skuamato@gmail.com>
2022-12-02 18:50:44 +0100

    initial commit

---
+++ TFT_sale_tool/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TFT_sale_tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CBAisling_Copy_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboHillockChoiceSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ComboHillockChoice.SelectedIndex == 2)
+            {
+                if(ComboHillockPercent.SelectedIndex == 0)
+                {
+                    ComboHillockPercent.SelectedIndex = 1;
+                }
+                //ComboHillockPercent.Items[4].IsEnabled = false
+            }
+            else
+            {
+                if(ComboHillockPercent != null)
+                {
+                    if (ComboHillockPercent.SelectedIndex == 1)
+                    {
+                        ComboHillockPercent.SelectedIndex = 0;
+                    }
+                }
+                
+                //ComboHillockPercent.Items[4].IsEnabled = false
+            }
+        }
+
+        private void CBTujenCurrency_Copy1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 set1 = new Window1();
+            set1.Show();
+        }
+
+        private void btnCopyClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            string resultText = System.Configuration.ConfigurationManager.AppSettings["baseText"] + "\n";
+            if (tabMain.SelectedItem == tabBetrayal)
+            {
+                resultText += System.Configuration.ConfigurationManager.AppSettings["textBetrayal"] + "\n";
+                if ((bool)CBAisling.IsChecked)
+                {
+                    resultText += "T4 Aisling " + TBAislingPrice.Text + " " + ComboCurrencyAisling.Text + "\n";
+                }
+                if ((bool)CBHillock.IsChecked)
+                {
+                    resultText += "Hillock " + ComboHillockChoice.Text + " " + ComboHillockPercent.Text + " " + TBHillockPrice.Text + " " + ComboCurrencyHillock.Text + "\n";
+                }
+            }
+            else if (tabMain.SelectedItem == tabExpedition)
+            {
+                resultText += System.Configuration.ConfigurationManager.AppSettings["textExpedition"] + "\n";
+            }
+            else if (tabMain.SelectedItem == tabHeist)
+            {
+                resultText += System.Configuration.ConfigurationManager.AppSettings["textHeist"] + "\n";
+            }
+            resultText += "IGN: " + System.Configuration.ConfigurationManager.AppSettings["ign"];
+            System.Windows.Clipboard.SetText(resultText);
+        }
+    }
+}
---
+++ TFT_sale_tool/Settings.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TFT_sale_tool
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            textBaseText.Text = System.Configuration.ConfigurationManager.AppSettings["baseText"];
+            textIGN.Text = System.Configuration.ConfigurationManager.AppSettings["ign"];
+            textBetrayal.Text = System.Configuration.ConfigurationManager.AppSettings["textBetrayal"];
+            textExpedition.Text = System.Configuration.ConfigurationManager.AppSettings["textExpedition"];
+            textHeist.Text = System.Configuration.ConfigurationManager.AppSettings["textExpedition"];
+        }
+
+        private void btnSaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+            System.Configuration.ConfigurationManager.AppSettings["baseText"] = textBaseText.Text;
+            System.Configuration.ConfigurationManager.AppSettings["ign"] = textIGN.Text;
+            System.Configuration.ConfigurationManager.AppSettings["textBetrayal"] = textBetrayal.Text;
+            System.Configuration.ConfigurationManager.AppSettings["textExpedition"] = textExpedition.Text;
+            System.Configuration.ConfigurationManager.AppSettings["textExpedition"] = textHeist.Text;
+            this.Close();
+        }
+
+        private void btnDiscardChanges_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBaseText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
